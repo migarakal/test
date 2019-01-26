24ee15ec996e0e50f1b52b0704f73f8c5b10a80b
Migara Kal <migarakal@gmail.com>
2019-01-26 18:44:31 +1100

    Added project source files.

---
+++ java-test-project/src/main/java/com/code/challenge/java_test_project/App.java
@@ -0,0 +1,13 @@
+package com.code.challenge.java_test_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
